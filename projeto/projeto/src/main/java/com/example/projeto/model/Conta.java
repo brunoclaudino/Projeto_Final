43e3dcf1ca9e6e5369ff7662fd02a3d8637021c0
TML45 <tassio.melo45@gmail.com>
2021-11-15 22:28:10 -0300

    Conta

---
+++ projeto/projeto/src/main/java/com/example/projeto/model/Conta.java
@@ -0,0 +1,98 @@
+package com.example.projeto.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+//import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//import javax.persistence.Table;
+
+
+//@Table(name = "conta")  //não sei se precisa
+//IDENTITY
+@Entity
+public class Conta implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	//@Column(name = "id_conta") //não sei se precisa
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer numero;
+	private Integer agencia;
+	private Double saldo;
+	private String dataAbertura;
+	
+	public Conta() {
+		super();
+	}
+
+	public Conta(Integer id, Integer numero, Integer agencia, Double saldo, String dataAbertura) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.agencia = agencia;
+		this.saldo = saldo;
+		this.dataAbertura = dataAbertura;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Integer getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(Integer agencia) {
+		this.agencia = agencia;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(String dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		return Objects.equals(id, other.id);
+	}	
+}
