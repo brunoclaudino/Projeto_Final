2c08ece8a68d82944d73e2b680a5743a94e9e484
Paulo R C Queiroz <queiroz1987@hotmail.com>
2021-11-24 10:42:22 -0300

    Atualização de transferencia atualizando saldo

--- src/main/java/com/bugsbuster/projectCaptainTech/service/TransferenciaService.java
+++ src/main/java/com/bugsbuster/projectCaptainTech/service/TransferenciaService.java
@@ -1,16 +1,20 @@
 package com.bugsbuster.projectCaptainTech.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bugsbuster.projectCaptainTech.model.Conta;
 import com.bugsbuster.projectCaptainTech.model.Transferencia;
+import com.bugsbuster.projectCaptainTech.repository.ContaRepository;
 import com.bugsbuster.projectCaptainTech.repository.TransferenciaRepository;
 
 @Service
-public class TransferenciaService implements InterfaceTransferenciaService{
+public class TransferenciaService implements InterfaceTransferenciaService {
 	@Autowired
 	TransferenciaRepository tranRepo;
+
+	@Autowired
+	ContaRepository contaRepo;
 
 	@Override
 	public Iterable<Transferencia> obterTodos() {
@@ -19,17 +23,24 @@ public class TransferenciaService implements InterfaceTransferenciaService{
 
 	@Override
 	public Iterable<Transferencia> obterPorDestino(int id) {
-		return  this.tranRepo.findByDestino(id);
+		return this.tranRepo.findByDestino(id);
 	}
 
 	@Override
-	public Transferencia criarTransferencia(Transferencia tran){
-		try {
-		
-		}catch(Exception e) {
-			System.out.println(e.toString());
+	public Transferencia criarTransferencia(Transferencia tran) {
+		Conta origem = contaRepo.getById(tran.getContaOrigem().getId_conta());
+		Conta destino = contaRepo.getById(tran.getContaDestino().getId_conta());
+		if(origem.getCliente().getAtivo() && destino.getCliente().getAtivo()) {
+			if(origem.getSaldo() - tran.getValor() >= -200.0) {
+				tran.getContaOrigem().setSaldo(origem.getSaldo()-tran.getValor());
+				tran.getContaDestino().setSaldo(tran.getValor());
+				contaRepo.save(origem); //postman não atualiza as tabelas de conta
+				contaRepo.save(destino);
+				return this.tranRepo.save(tran);
+			}
 		}
-		return this.tranRepo.save(tran);
+		return null;
+		
 	}
 
 	@Override
